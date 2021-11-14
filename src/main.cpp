a01cf491a7a05ca72eae0aac2c3777bd749929af
Ole-André Rodlie <ole.andre.rodlie@gmail.com>
2021-11-14 21:13:13 +0100

    add main

---
+++ src/main.cpp
@@ -0,0 +1,39 @@
+/*
+#
+# Natron Plug-in Manager
+#
+# Copyright (c) 2021 Ole-André Rodlie. All rights reserved.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>
+#
+*/
+
+#include "app.h"
+
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    QApplication::setApplicationName(APP_NAME);
+    QApplication::setApplicationVersion(APP_VERSION);
+    QApplication::setApplicationDisplayName(QObject::tr("Natron Plug-in Manager"));
+    QApplication::setOrganizationName(APP_ORG);
+    QApplication::setOrganizationDomain(APP_DOMAIN);
+
+    NatronPluginManager app;
+    app.show();
+
+    return a.exec();
+}
